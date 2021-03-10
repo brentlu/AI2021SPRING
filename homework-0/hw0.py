945c7b5c7be4e27d007a1c744f1e52b4974bed94
Brent Lu <brent.lu@intel.com>
2021-03-10 18:13:22 +0800

    hw0: submission
    Score: 100
    
    Implement BFS walk to check if the graph is bipartite.

--- homework-0/hw0.py
+++ homework-0/hw0.py
@@ -15,9 +15,28 @@ class Graph:
         Returns:
           True/False
         """
-        #TODO
-        pass
+        color = [0] * (self.size+1)
+        queue = []
 
+        # insert node 1
+        color[1] = 1
+        queue.append(int(1))
+
+        # BFS walk
+        while queue:
+            u = queue.pop()
+
+            for v in self.adjacencyList[u]:
+                if color[v] == color[u]:
+                    return False
+                elif color[v] == 0:
+                    if color[u] == 1:
+                        color[v] = 2
+                    else:
+                        color[v] = 1
+                    queue.append(v)
+
+        return True
 
 if __name__=="__main__":
 
